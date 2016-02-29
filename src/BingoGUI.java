9392e6afb05e15b15a9466d84a0e3c06a3343385
beastben13 <beennn1@yahoo.com>
2016-02-28 22:26:51 -0500

    Added files via upload

--- src/BingoGUI.java
+++ src/BingoGUI.java
@@ -1,5 +1,139 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import java.awt.GridBagLayout;
+import javax.swing.JTextField;
+import java.awt.GridBagConstraints;
+import java.awt.GridLayout;
+import javax.swing.JLabel;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import java.awt.FlowLayout;
+import com.jgoodies.forms.layout.FormSpecs;
+import java.awt.Insets;
+import javax.swing.SwingConstants;
 
 public class BingoGUI
 {
 
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args)
+	{
+		EventQueue.invokeLater(new Runnable()
+		{
+			public void run()
+			{
+				try
+				{
+					BingoGUI window = new BingoGUI();
+					window.frame.setVisible(true);
+				} catch (Exception e)
+				{
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public BingoGUI()
+		{
+			initialize();
+		}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize()
+	{
+		frame = new JFrame();
+		frame.setBounds(100, 100, 655, 360);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		frame.getContentPane().setLayout(gridBagLayout);
+		
+		JLabel lblCurrentCard = new JLabel("Current Card:");
+		lblCurrentCard.setHorizontalAlignment(SwingConstants.LEFT);
+		GridBagConstraints gbc_lblCurrentCard = new GridBagConstraints();
+		gbc_lblCurrentCard.gridwidth = 2;
+		gbc_lblCurrentCard.insets = new Insets(0, 0, 5, 5);
+		gbc_lblCurrentCard.gridx = 1;
+		gbc_lblCurrentCard.gridy = 1;
+		frame.getContentPane().add(lblCurrentCard, gbc_lblCurrentCard);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel.gridx = 3;
+		gbc_lblNewLabel.gridy = 1;
+		frame.getContentPane().add(lblNewLabel, gbc_lblNewLabel);
+		
+		JLabel lblYourCards = new JLabel("Your cards:");
+		lblYourCards.setHorizontalAlignment(SwingConstants.LEFT);
+		GridBagConstraints gbc_lblYourCards = new GridBagConstraints();
+		gbc_lblYourCards.gridwidth = 2;
+		gbc_lblYourCards.insets = new Insets(0, 0, 5, 5);
+		gbc_lblYourCards.gridx = 1;
+		gbc_lblYourCards.gridy = 3;
+		frame.getContentPane().add(lblYourCards, gbc_lblYourCards);
+		
+		JLabel lblNewLabel_1 = new JLabel("New label");
+		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
+		gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_1.gridx = 3;
+		gbc_lblNewLabel_1.gridy = 3;
+		frame.getContentPane().add(lblNewLabel_1, gbc_lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("New label");
+		GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
+		gbc_lblNewLabel_2.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_2.gridx = 5;
+		gbc_lblNewLabel_2.gridy = 3;
+		frame.getContentPane().add(lblNewLabel_2, gbc_lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("New label");
+		GridBagConstraints gbc_lblNewLabel_3 = new GridBagConstraints();
+		gbc_lblNewLabel_3.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_3.gridx = 7;
+		gbc_lblNewLabel_3.gridy = 3;
+		frame.getContentPane().add(lblNewLabel_3, gbc_lblNewLabel_3);
+		
+		JLabel lblNewLabel_4 = new JLabel("New label");
+		GridBagConstraints gbc_lblNewLabel_4 = new GridBagConstraints();
+		gbc_lblNewLabel_4.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_4.gridx = 9;
+		gbc_lblNewLabel_4.gridy = 3;
+		frame.getContentPane().add(lblNewLabel_4, gbc_lblNewLabel_4);
+		
+		JLabel lblNewLabel_5 = new JLabel("New label");
+		GridBagConstraints gbc_lblNewLabel_5 = new GridBagConstraints();
+		gbc_lblNewLabel_5.insets = new Insets(0, 0, 5, 0);
+		gbc_lblNewLabel_5.gridx = 11;
+		gbc_lblNewLabel_5.gridy = 3;
+		frame.getContentPane().add(lblNewLabel_5, gbc_lblNewLabel_5);
+		
+		JLabel lblPionts = new JLabel("Pionts:");
+		GridBagConstraints gbc_lblPionts = new GridBagConstraints();
+		gbc_lblPionts.insets = new Insets(0, 0, 0, 5);
+		gbc_lblPionts.gridx = 9;
+		gbc_lblPionts.gridy = 7;
+		frame.getContentPane().add(lblPionts, gbc_lblPionts);
+		
+		JLabel label = new JLabel("0");
+		GridBagConstraints gbc_label = new GridBagConstraints();
+		gbc_label.gridx = 11;
+		gbc_label.gridy = 7;
+		frame.getContentPane().add(label, gbc_label);
+	}
 }
